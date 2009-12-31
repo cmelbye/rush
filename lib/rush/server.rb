8a772d0c41ebaf6a599f5c92069fcd97a73d0074
Charlie Melbye <charlie@yourwiki.net>
2009-12-30 23:41:51 -0600

    Switch to a Rack application and Thin instead of Mongrel.

--- lib/rush/server.rb
+++ lib/rush/server.rb
@@ -1,85 +1,51 @@
 require 'rubygems'
-require 'mongrel'
-require 'base64'
+require 'thin'
 
-# Mongrel handler that translates the incoming HTTP request into a
+# Rack handler that translates the incoming HTTP request into a
 # Rush::Connection::Local call.  The results are sent back across the wire to
 # be decoded by Rush::Connection::Remote on the other side.
-class RushHandler < Mongrel::HttpHandler
-	def process(request, response)
-		params = {}
-		request.params['QUERY_STRING'].split("?").last.split("&").each do |tuple|
-			key, value = tuple.split("=")
-			params[key.to_sym] = value
-		end
-
-		unless authorize(request.params['HTTP_AUTHORIZATION'])
-			response.start(401) do |head, out|
-			end
-		else
-			payload = request.body.read
-
-			without_action = params
-			without_action.delete(params[:action])
-
-			msg = sprintf "%-20s", params[:action]
-			msg += without_action.inspect
-			msg += " + #{payload.size} bytes of payload" if payload.size > 0
-			log msg
-
-			params[:payload] = payload
-
-			begin
-				result = box.connection.receive(params)
-
-				response.start(200) do |head, out|
-					out.write result
-				end
-			rescue Rush::Exception => e
-				response.start(400) do |head, out|
-					out.write "#{e.class}\n#{e.message}\n"
-				end
-			end
-		end
-	rescue Exception => e
-		log e.full_display
-	end
-
-	def authorize(auth)
-		unless m = auth.match(/^Basic (.+)$/)
-			log "Request with no authorization data"
-			return false
-		end
-
-		decoded = Base64.decode64(m[1])
-		user, password = decoded.split(':', 2)
-
-		if user.nil? or user.length == 0 or password.nil? or password.length == 0
-			log "Malformed user or password"
-			return false
-		end
-
-		if password == config.passwords[user]
-			return true
-		else
-			log "Access denied to #{user}"
-			return false
-		end
-	end
-
-	def box
-		@box ||= Rush::Box.new('localhost')
-	end
-
-	def config
-		@config ||= Rush::Config.new
-	end
-
-	def log(msg)
-		File.open('rushd.log', 'a') do |f|
-			f.puts "#{Time.now.strftime('%Y-%m-%d %H:%I:%S')} :: #{msg}"
-		end
-	end
+class RushHandler
+  def call(env)
+    req = Rack::Request.new(env)
+    params = req.GET
+    payload = req.body.read
+    
+    params.each do |k,v|
+      params.delete(k)
+      params[k.to_sym] = v if k.respond_to? :to_sym
+    end
+    
+    without_action = params
+    without_action.delete(params[:action])
+    
+    msg = sprintf("%-20s", params[:action])
+    msg += without_action.inspect
+    msg += " + #{payload.size} bytes of payload" if payload.size > 0
+    log msg
+    
+    params[:payload] = payload
+    
+    begin
+      result = box.connection.receive(params)
+      response = result
+    rescue Rush::Exception => e
+      response = "#{e.class}\n#{e.message}\n"
+    end
+    
+    [200, {'Content-Type' => 'text/plain'}, [response]]
+  rescue Exception => e
+    log e.full_display
+  end
+  
+  def box
+    @box ||= Rush::Box.new('localhost')
+  end
+  
+  def log(msg)
+    File.open('rushd.log', 'a') do |f|
+      f.puts "#{Time.now.strftime('%Y-%m-%d %H:%I:%S')} :: #{msg}"
+    end
+  end
 end
 
 # A container class to run the Mongrel server for rushd.
@@ -87,13 +53,18 @@ class RushServer
 	def run
 		host = "127.0.0.1"
 		port = Rush::Config::DefaultPort
-
-		rushd = RushHandler.new
-		rushd.log "rushd listening on #{host}:#{port}"
-
-		h = Mongrel::HttpServer.new(host, port)
-		h.register("/", rushd)
-		h.run.join
+    rushd = Rack::Builder.app do
+      use Rack::Auth::Basic, "rushd" do |username, password|
+        config = Rush::Config.new
+        password == config.passwords[username]
+      end
+      
+      run RushHandler.new
+    end
+    
+    ::Thin::Logging.silent = true
+    Rack::Handler::Thin.run(rushd, :Host => host, :Port => port)
+    rushd.log "rushd listening on #{host}:#{port}"
 	end
 end
 
